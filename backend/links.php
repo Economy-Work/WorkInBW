a152f0aea7680473c8826779566bfb1b51cf0388
reinw <reinwaldfelix79@gmail.com>
2023-04-15 18:12:37 +0200

    added "Make it in Germany" to links

--- backend/links.php
+++ backend/links.php
@@ -19,18 +19,19 @@ require_once __DIR__.'/config.php';
   <div class="container px-4 pt-3 pb-5">
     <h2 class="pb-2 mb-2 border-bottom">Links</h2>
     <ul>
-        <li><a href="https://www.auswaertiges-amt.de/en" target="_blank">Foreign Office Germany</a></li>
-        <li><a href="https://www.anerkennung-in-deutschland.de/en/interest/finder/profession?type=list" target="_blank">Check for recognition of your profession</a></li>
-        <li><a href="https://visa.diplo.de/en/index.html#/vib/schnelltest" target="_blank">Quick Check Visa</a></li>
-        <li><a href="https://www.diplo.de/addresses" target="_blank">Check for additional Requierments</a></li>
-        <li><a href="https://videx-national.diplo.de/videx/visum-erfassung/#/videx-langfristiger-aufenthalt" target="_blank">Visa Form</a></li>
-        <li><a href="https://www.engelvoelkers.com/en-de/properties/rent-apartment/baden-wurttemberg/" target="_blank">Rent a Flat (Engel & Voelkers)</a></li>
-        <li><a href="https://www.nestpick.com/baden-wurttemberg/" target="_blank">Rent a Flat (Nestpick)</a></li>
-        <li><a href="https://www.tourism-bw.com/experience/top-attractions" target="_blank">Attractions in Baden-Wuertemberg</a></li>
-        <li><a href="https://www.bwegt.de/" target="_blank">Train Tickets (bwegt, only german)</a></li>
-        <li><a href="https://www.bahn.com/en" target="_blank">Train Tickets (Deutsche Bahn)</a></li>
-        <li><a href="https://www.languageinternational.com/german-courses-baden-wurttemberg-1912" target="_blank">German courses in Baden-Wuertemberg</a></li>
-        <li><a href="https://www.deutschakademie.de/online-deutschkurs/english" target="_blank">Online German courses (DeutscheAkademie)</a></li>
+      <li><a href="https://www.make-it-in-germany.com/en/" target="_blank">Make it in Germany</a></li>
+      <li><a href="https://www.auswaertiges-amt.de/en" target="_blank">Foreign Office Germany</a></li>
+      <li><a href="https://www.anerkennung-in-deutschland.de/en/interest/finder/profession?type=list" target="_blank">Check for recognition of your profession</a></li>
+      <li><a href="https://visa.diplo.de/en/index.html#/vib/schnelltest" target="_blank">Quick Check Visa</a></li>
+      <li><a href="https://www.diplo.de/addresses" target="_blank">Check for additional Requierments</a></li>
+      <li><a href="https://videx-national.diplo.de/videx/visum-erfassung/#/videx-langfristiger-aufenthalt" target="_blank">Visa Form</a></li>
+      <li><a href="https://www.engelvoelkers.com/en-de/properties/rent-apartment/baden-wurttemberg/" target="_blank">Rent a Flat (Engel & Voelkers)</a></li>
+      <li><a href="https://www.nestpick.com/baden-wurttemberg/" target="_blank">Rent a Flat (Nestpick)</a></li>
+      <li><a href="https://www.tourism-bw.com/experience/top-attractions" target="_blank">Attractions in Baden-Wuertemberg</a></li>
+      <li><a href="https://www.bwegt.de/" target="_blank">Train Tickets (bwegt, only german)</a></li>
+      <li><a href="https://www.bahn.com/en" target="_blank">Train Tickets (Deutsche Bahn)</a></li>
+      <li><a href="https://www.languageinternational.com/german-courses-baden-wurttemberg-1912" target="_blank">German courses in Baden-Wuertemberg</a></li>
+      <li><a href="https://www.deutschakademie.de/online-deutschkurs/english" target="_blank">Online German courses (DeutscheAkademie)</a></li>
     </ul>
   </div>
 </main>
