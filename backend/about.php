b981a13ae0be0230977c443260c47b140abf0adc
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 18:32:23 +0200

    create basic about page

---
+++ backend/about.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__.'/config.php';
+
+?><!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>About</title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/assets/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<main>
+  <?php include __DIR__.'/parts/navbar.php'; ?>
+
+  <div class="container px-4 pt-3 pb-5">
+    <h2 class="pb-2 mb-2 border-bottom">About</h2>
+    <p>
+      This Project was created by
+      <a href="https://github.com/ridhimagarg" target="_blank">Ridhima</a>,
+      <a href="https://github.com/akshat4112" target="_blank">Akshat</a>,
+      <a href="https://github.com/megagmbh-mega" target="_blank">Felix</a> and
+      <a href="https://github.com/anthemaker" target="_blank">Anton</a>
+      during the
+      <a href="https://mesh-stuttgart.de/" target="_blank"><b>Mesh 2023</b></a>
+      Hackathon in Böblingen, Germany.
+    </p>
+  </div>
+</main>
+
+  <script src="/assets/js/bootstrap.js"></script>
+</body>
+</html>
