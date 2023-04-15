207b4494ac54659302bb60393087bd88aab054df
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 17:14:01 +0200

    improve navbar

--- backend/parts/navbar.php
+++ backend/parts/navbar.php
@@ -3,13 +3,13 @@
     <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
   </ul>  
 
-  <a href="/" class="d-flex col-md-12 justify-content-md-center flex-md-grow-1 align-items-center col-md-auto col-md-3 mb-2 text-dark text-decoration-none w-auto">
+  <a href="/" class="d-flex col-md-12 justify-content-md-center flex-md-grow-1 align-items-center col-md-auto col-md-3 text-dark text-decoration-none w-auto">
     <img src="/assets/img/logo.svg" height="55" alt="WorkinBW">
   </a>
 
   <div class="col-3 text-end flex-grow-1 flex-md-grow-0">
     <?php if($_SESSION['loggedin']){ ?>
-      <a href="/logout.php" class="btn btn-outline-primary me-2">Logout</a>
+      <a href="/logout.php" class="btn btn-outline-dark me-2">Logout</a>
     <?php }else{ ?>
       <a href="/login.php" class="btn btn-outline-dark me-2">Login</a>
       <a href="/signup.php" class="btn btn-dark me-2">Sign Up</a>
