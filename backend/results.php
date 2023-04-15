a6625218ee47c2f7c11202d09ea83c6cb46e0670
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-16 01:08:38 +0200

    add "resources" box to results page

--- backend/results.php
+++ backend/results.php
@@ -92,6 +92,17 @@ require_once __DIR__.'/config.php';
       </table>
     </div>
 
+    <div class="card w-100 my-5">
+      <div class="card-body">
+        <h5 class="card-title">Thank you for completing the assignments!</h5>
+        <p class="card-text">Learn more about how to get started in THE LÄND and find accomondations, visas, transportation and more!</p>
+        <a href="/links.php" class="btn btn-dark">
+          View Resources
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+        </a>
+      </div>
+    </div>
+
   </div>
 </main>
 
