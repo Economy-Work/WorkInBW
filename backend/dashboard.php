54d8f07fc62c2dd4aea65d34046d8a27c28fe820
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 19:35:34 +0200

    fix typo/improve text

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -79,7 +79,7 @@ if(!$_SESSION['loggedin']){
         <div class="card w-100">
           <div class="card-body">
             <h5 class="card-title">Get Started in THE LÄND</h5>
-            <p class="card-text">Learn more about accomondations, visas, transportaion and more in Germany!</p>
+            <p class="card-text">Learn more about accomondations, visas, transportation and more in Baden-Württemberg!</p>
             <a href="/links.php" class="btn btn-dark">
               View Resources
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
