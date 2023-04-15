b60d596a2b048941f4cc53979ad626db24f4a66a
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 10:50:28 +0200

    create skeleton for landingpage

---
+++ backend/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Interview</title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/assets/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<main>
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2 px-3 mb-4 border-bottom" style="background: #FDFF00;">
+   <ul class="nav col-md-3 mb-2 mb-md-0">
+      <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+    </ul>  
+  
+    <a href="/" class="d-flex col-12 justify-content-center flex-grow-1 align-items-center col-md-auto col-md-3 mb-2 text-dark text-decoration-none">
+      <img src="/assets/img/logo.svg" height="55" alt="WorkinBW">
+    </a>
+
+    <div class="col-md-3 text-end">
+      <button type="button" class="btn btn-outline-primary me-2">Login</button>
+    </div>
+  </header>
+
+  <div class="container px-4 py-5">
+    
+  </div>
+</main>
+
+  <script src="/assets/js/bootstrap.js"></script>
+</body>
+</html>
