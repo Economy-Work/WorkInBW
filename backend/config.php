5f610173d4a08a2ba4452cb33d5814d00bdd995f
reinw <reinwaldfelix79@gmail.com>
2023-04-15 21:45:31 +0200

    added Videos and video Key

--- backend/config.php
+++ backend/config.php
@@ -49,16 +49,19 @@ $interview_steps = [ // subject to change
         'type' => 'text',
         'name' => 'tell_yourself',
         'question' => 'Tell me something about yourself.'
+        'video' => 'Starting.mp4'
       ],
       [
         'type' => 'text',
         'name' => 'working_bc',
         'question' => 'Why do you want to work in Baden-Württemberg?'
+        'video' => 'WhyWorkHere.mp4'
       ],
       [
         'type' => 'text',
         'name' => 'strengths',
         'question' => 'What are your greatest strengths?'
+        'video' => 'YourStrenghts.mp4'
       ],
       [
         'type' => 'text',
@@ -79,11 +82,13 @@ $interview_steps = [ // subject to change
         'type' => 'text',
         'name' => 'greatest_accomp',
         'question' => 'What is your greatet accomplishment?'
+        'video' => 'GreatestAccomp.mp4'
       ],
       [
         'type' => 'text',
         'name' => 'position',
         'question' => 'What are you looking for a new position?'
+        'video' => 'Position.mp4'
       ],
       [
         'type' => 'text',
