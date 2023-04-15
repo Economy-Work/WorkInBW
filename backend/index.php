e7e67c4e6c3654b35ba2983409d4f7b4463ff3eb
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 18:35:29 +0200

    add dynamic navbar to landingpage

--- backend/index.php
+++ backend/index.php
@@ -11,25 +11,13 @@
 <body  style="background: #FDFF00;">
 
 <main>
-  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2 px-3 mb-4 border-bottom" style="background: #FDFF00;">
-   <ul class="nav col-md-3 mb-2 mb-md-0">
-      <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-    </ul>  
-  
-    <a href="/" class="d-flex col-12 justify-content-center flex-grow-1 align-items-center col-md-auto col-md-3 mb-2 text-dark text-decoration-none">
-      <img src="assets/img/logo.svg" height="55" alt="WorkinBW">
-    </a>
-
-    <div class="col-md-3 text-end">
-      <button type="button" class="btn btn-outline-dark me-2">Login</button>
-    </div>
-  </header>
+  <?php include __DIR__.'/parts/navbar.php'; ?>
 
   <div class="container px-4 py-5 pt-3">
 
     <h1 class="pb-2 mb-2 text-center border-bottom">Welcome to WorkinBW</h1>
     <div class="row">
-      <img src="/assets/img/logo.svg" class="col-md-8 mx-auto img-fluid" alt="WorkinBW">
+      <img src="/assets/img/logo.svg" class="col-md-7 mx-auto img-fluid" alt="WorkinBW">
     </div>
     <div class="container">
       <div class="row mt-3 g-5" >
