94818e7bc8d1f229cc526956573e1d8b20e14374
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 19:17:06 +0200

    add arrow to CTA button

--- backend/index.php
+++ backend/index.php
@@ -42,7 +42,10 @@
       </div>
       <div class="row mt-3 gx-5">
         <div class="col-12">
-          <a href="/signup.php" class="btn btn-lg btn-block btn-dark p-2 w-100">Let's get started!</a>
+          <a href="/signup.php" class="btn btn-lg btn-block btn-dark p-2 w-100">
+            Let's get started!
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/></svg>
+          </a>
         </div>
       </div>
     </div>
