0752e063f2aae2d23c6bdc2dd142d0990442c06a
An <18481195+AnTheMaker@users.noreply.github.com>
2023-04-15 11:29:16 +0200

    put navbar code into seperate file for easier maintainability

---
+++ backend/parts/navbar.php
@@ -0,0 +1,13 @@
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2 px-3 mb-4 border-bottom" style="background: #FDFF00;">
+  <ul class="nav col-md-3 mb-2 mb-md-0">
+    <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+  </ul>  
+
+  <a href="/" class="d-flex col-12 justify-content-center flex-grow-1 align-items-center col-md-auto col-md-3 mb-2 text-dark text-decoration-none">
+    <img src="/assets/img/logo.svg" height="55" alt="WorkinBW">
+  </a>
+
+  <div class="col-md-3 text-end">
+    <button type="button" class="btn btn-outline-primary me-2">Login</button>
+  </div>
+</header>
