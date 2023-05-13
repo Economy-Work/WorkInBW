cb7d7ec6a8c1d0791f92c872c956f23d6ef54e63
anthemaker <18481195+AnTheMaker@users.noreply.github.com>
2023-05-13 16:45:13 +0200

    create pages redirect

--- backend/parts/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="py-3 my-4">
-  <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-    <li class="nav-item"><a href="/" class="nav-link px-2 text-body-secondary">Home</a></li>
-    <li class="nav-item"><a href="/links.php" class="nav-link px-2 text-body-secondary">Further Links</a></li>
-    <li class="nav-item"><a href="/about.php" class="nav-link px-2 text-body-secondary">About the Project</a></li>
-  </ul>
-  <p class="text-center text-body-secondary">Made in <b>THE LÄND</b></p>
-</footer>
